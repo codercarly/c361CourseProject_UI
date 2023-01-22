d80d6373fc44eb2135f76c3e6d6229dcb0ce6d17
codercarly <codercarly@gmail.com>
2023-01-22 18:17:31 -0500

    add recipe scraper functionality

---
+++ RecipeScraper/recipeScraper.go
@@ -0,0 +1,49 @@
+package recipescraper
+
+// import (
+// 	"bytes"
+// 	"fmt"
+// 	"io"
+
+// 	"github.com/PuerkitoBio/goquery"
+// 	"github.com/kkyr/go-recipe/pkg/recipe"
+// )
+
+// type httpClient interface {
+// 	Get(url string) ([]byte, error)
+// }
+
+// var client httpClient = http.NewClient()
+
+// // ScrapeURL retrieves the source at the provided url and returns a
+// // Scraper that scrapes recipe data from the retrieved HTML.
+// func ScrapeURL(urlStr string) (recipe.Scraper, error) {
+// 	body, err := client.Get(urlStr)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to GET url: %w", err)
+// 	}
+
+// 	return ScrapeHTML(urlStr, bytes.NewReader(body))
+// }
+
+// // ScrapeHTML returns a Scraper that scrapes recipe data from the provided HTML.
+// // the urlStr is used to determine if a specific scraper should be used, otherwise
+// // a generic scraper is used.
+// func ScrapeHTML(urlStr string, body io.Reader) (recipe.Scraper, error) {
+// 	doc, err := goquery.NewDocumentFromReader(body)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to parse HTML document: %w", err)
+// 	}
+
+// 	host := url.GetHost(urlStr)
+// 	if scraper, ok := hostToScraper[host]; ok {
+// 		return scraper(doc)
+// 	}
+
+// 	scraper, err := schema.NewRecipeScraper(doc)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to get new schema scraper: %w", err)
+// 	}
+
+// 	return scraper, nil
+// }
--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	n "c361CourseProject_UI/Navigation"
+	"github.com/kkyr/go-recipe/pkg/recipe"
 )
 
 func main() {
 
-	n.AppTitleIntro()
-	n.PickAnIngredient()
-	n.ChooseYourRecipe("CHICKEN")
-	n.NAVChooseYourRecipe()
-	n.Recipe("Chicken Fettucini Alfredo")
-	n.NAVRecipe()
+	// n.AppTitleIntro()
+	// n.PickAnIngredient()
+	// n.ChooseYourRecipe("CHICKEN")
+	// n.NAVChooseYourRecipe()
+	// n.Recipe("Chicken Fettucini Alfredo")
+	// n.NAVRecipe()
+
+	url := "https://minimalistbaker.com/quick-pickled-jalapenos/"
+
+	recipe, err := recipe.ScrapeURL(url)
+	if err != nil {
+		// handle err
+	}
+
+	ingredients, ok := recipe.Ingredients()
+	if !ok {
+		println("Not ok")
+	}
+	instructions, ok := recipe.Instructions()
+	if !ok {
+		println("Not ok")
+	}
+
+	for i := range ingredients {
+		println(i)
+	}
+
+	for i := range instructions {
+		println(i)
+	}
+	println(recipe.Author())
+	println(recipe.CookTime())
+	println(recipe.Cuisine())
+	println(recipe.Description())
+	println(recipe.Yields())
 }
